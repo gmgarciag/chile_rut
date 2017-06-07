0159892715d77f9d62e41a6a5f359b76914ef065
Gonzalo Garcia <gmgarcia@uc.cl>
2017-06-06 23:04:20 -0400

    delete example

--- chile-rut/chile-rut.py
+++ chile-rut/chile-rut.py
@@ -1,5 +1,5 @@
 import re
-from itertools import *
+import itertools
 
 def validate_rut(rut):
     number_sum = 0
@@ -7,7 +7,7 @@ def validate_rut(rut):
     if not validate_rut_format(rut): return 1
     splited_rut = clean_rut.split("-")
     inv_rut = splited_rut[0][::-1]
-    coefficents = cycle(range(2,8))
+    coefficents = itertools.cycle(range(2,8))
     for number in inv_rut:
         number_sum += next(coefficents) * int(number)
     verification_digit = 11 - (number_sum % 11)
@@ -23,6 +23,3 @@ def validate_rut_format(rut):
     search_finish = re.search("-[0-9kK]$", rut)
     search_score = re.search("-{1}", rut)
     return search_start and search_score and search_finish
-
-if __name__ == "__main__":
-    print validate_rut("18.023.503-5")
